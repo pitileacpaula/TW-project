ab94cab8ad61ba12affa561b3e66253e66bb8e3c
Robert <robert.98.nagy@gmail.com>
2020-11-18 14:56:18 +0200

    Set the map default location on Romania

--- src/Home.js
+++ src/Home.js
@@ -207,8 +207,8 @@ export default function ProminentAppBar() {
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
-  const [center, setCenter] = useState([-94.9065, 38.9884]);
-    const [zoom, setZoom] = useState(9);
+  const [center, setCenter] = useState([25.20, 	45.94]);
+    const [zoom, setZoom] = useState(6.8);
     const [showLayer1, setShowLayer1] = useState(true);
     const [showLayer2, setShowLayer2] = useState(true);
   return (
@@ -288,36 +288,11 @@ export default function ProminentAppBar() {
                       source={osm()}
                       zIndex={0}
                     />
-                    {showLayer1 && (
-                      <VectorLayer
-                        source={vector({ features: new GeoJSON().readFeatures(geojsonObject, { featureProjection: get('EPSG:3857') }) })}
-                        style={styles.MultiPolygon}
-                      />
-                    )}
-                    {showLayer2 && (
-                      <VectorLayer
-                        source={vector({ features: new GeoJSON().readFeatures(geojsonObject2, { featureProjection: get('EPSG:3857') }) })}
-                        style={styles.MultiPolygon}
-                      />
-                    )}
                   </Layers>
                   <Controls>
                     <FullScreenControl />
                   </Controls>
                 </Map>
-                <div>
-                  <input
-                    type="checkbox"
-                    checked={showLayer1}
-                    onChange={event => setShowLayer1(event.target.checked)}
-                  /> Johnson County
-                </div>
-                <div>
-                  <input
-                    type="checkbox"
-                    checked={showLayer2}
-                    onChange={event => setShowLayer2(event.target.checked)}
-                  /> Wyandotte County</div>
                 </Typography>
         </Typography>
       </Container>
